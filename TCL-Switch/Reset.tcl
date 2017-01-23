665106a1af2d353ecf7f601d46afb3b8c4a5cc94
yolmant <yolmat24@outlook.com>
2017-01-22 16:50:59 -0800

    Create Reset.tcl
    this script will reset the switch and erase all Vlan file and the starting configuration

---
+++ TCL-Switch/Reset.tcl
@@ -0,0 +1,14 @@
+#this script will successfully work in switches 2960. for other switches like 3650 erase the command "sdm prefer dual-ipv4-and-ipv6 routing"
+
+puts [ open flash:Reset.tcl w+ ] {
+typeahead "\n"
+copy running-config startup-config
+typeahead "\n"
+erase startup-config
+delete /force vlan.dat
+delete /force multiple-fs
+ios_config "sdm prefer dual-ipv4-and-ipv6 routing"
+typeahead "\n"
+puts "Switch in 1 minute. please relax."
+reload in 1 RESET.TCL SCRIPT RUN
+}
