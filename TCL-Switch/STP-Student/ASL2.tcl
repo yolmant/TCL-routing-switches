47573bd1c3c5953aaaaa2fd67846e9ecc092836f
yolmant <yolmat24@outlook.com>
2017-01-24 19:25:38 -0800

    Update ASL2.tcl

--- TCL-Switch/STP-Student/ASL2.tcl
+++ TCL-Switch/STP-Student/ASL2.tcl
@@ -1,5 +1,11 @@
+#for this script we are using as ASL switches 3560
+
 puts [ open flash:Lab4 w+ ] {
 
 #VTP version 3 configuration
 ios_config "vtp domain SWLAB" "vtp version 3" "vtp mode server"
+
+#change the cost on the port 12
+ios_config "interface g1/0/7" "spanning-tree cost 12" "exit"
+
 }
