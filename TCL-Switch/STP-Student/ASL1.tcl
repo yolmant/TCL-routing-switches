c2bbb51a82974fa2c6bb82fe9a44e912217d6c79
yolmant <yolmat24@outlook.com>
2017-01-24 19:34:15 -0800

    Update ASL1.tcl

--- TCL-Switch/STP-Student/ASL1.tcl
+++ TCL-Switch/STP-Student/ASL1.tcl
@@ -1,5 +1,14 @@
+#for this script we are using as ASL switches 3560
 puts [ open flash:Lab4 w+ ] {
 
 #VTP version 3 configuration
 ios_config "vtp domain SWLAB" "vtp version 3" "vtp mode server"
+
+#from here we will apply a serie of command that will compare the congence between the no edge port and the portfast
+#debug to obser the convergence of using a no edge port
+debug spanning-tree events
+ios_config "interface g1/0/6" "switchport mode access" "switchport access vlan 120" "no shutdown"
+
+#activate the portfast 
+ios_config "interface g1/0/6" "spanning-tree portfast" "shutdown"
 }
