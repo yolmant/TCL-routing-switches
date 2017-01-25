a816e867bdc46eaecdda723f7ac69c90d65800b8
yolmant <yolmat24@outlook.com>
2017-01-24 18:50:53 -0800

    Update DSL2.tcl

--- TCL-Switch/STP-Student/DSL2.tcl
+++ TCL-Switch/STP-Student/DSL2.tcl
@@ -2,4 +2,7 @@ puts [ open flash:Lab4 w+ ] {
 
 #VTP version 3 configuration
 ios_config "vtp domain SWLAB" "vtp version 3" "vtp mode server"
+
+#configuring SPT primary for vlan 110,120 and secondary for vlan 99,100
+ios_config "spanning-tree vlan 99,100 root primary" "spanning-tree vlan 110,120 root secondary" "exit"
 }
