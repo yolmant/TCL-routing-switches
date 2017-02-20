3d41e85a822554e549b454a558caf098d9c93cb5
Yolman Torrez <yolmat24@outlook.com>
2017-02-19 20:17:05 -0800

    Create ASL1.tcl

---
+++ TCL-Switch/EthernetChannel/ASL1.tcl
@@ -0,0 +1,14 @@
+#this shell script will configure EtherChannel PAgP and LACP
+puts [ open flash:EtherChannel w+ ] {
+
+#port-channel 1 as PAgP
+ios_config "interface range g1/0/11-12" "shutdown" "channel-group 1 mode desirable" "no shutdown" "exit"
+
+#port-channel 2 as LACP
+ios_config "interface range g1/0/7-8" "shutdown" "channel-group 2 mode active" "no shutdown" "exit"
+
+#creating port-channels to misconfigure the network
+ios_config "interface range g1/0/9-10" "shutdown" "channel-group 3 mode active" "no shutdown" "exit"
+
+#configure load-balancing
+ios_config "port-channel load-balance src-dst-ip"
