488e37629a01600828c7ff33a5d637c7339d6a1f
Yolman Torrez <yolmat24@outlook.com>
2017-03-01 13:41:23 -0800

    Update DSL1.tcl

--- TCL-Switch/Inter-VLAN Routing/DSL1.tcl
+++ TCL-Switch/Inter-VLAN Routing/DSL1.tcl
@@ -8,3 +8,10 @@ ios_config "interface vlan 99" "ip address 10.1.99.1 255.255.255.0" "no shutdown
 
 #address the interface loopback
 ios_config "interface Lo1" "ip address 172.16.1.1 255.255.255.0" "no shutdown" "exit"
+
+#convert the link with DSL2 to layer 3
+ios_config "interface range g1/0/11-12" "no switchport" "channel-group 2 mode desirable" "no shutdown" "exit"
+ios_config "interface port-channel 2" "ip address 176.16.12.1 255.255.255.252" "no shutdown" "exit"
+
+#configuring a static default route
+ios_config "ip route 0.0.0.0 0.0.0.0 port-channel 2 172.16.12.2"
